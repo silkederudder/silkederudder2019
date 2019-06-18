97d1a036e078dbc641d72846dd189489a6bf7030
Silke Derudder <silkederudder@hotmail.com>
2019-06-18 10:47:55 +0200

    :sparkles: Retrieve all needed data for the about page and create basic structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,78 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Helmet from 'react-helmet'
+import { graphql } from "gatsby";
+import get from 'lodash/get'
+import Img from "gatsby-image";
 
-const About = () => (
-  <Layout>
-    <SEO title="About" />
-    <h1>About page</h1>
-    <p>hello world</p>
-  </Layout>
-)
+class AboutPage extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const about = get(this, 'props.data.allContentfulPerson.edges')[0].node
 
-export default About
+    return (
+      <Layout>
+        <SEO title="About" keywords={[`about`, `silke derudder`, `design`, `development`, `contact`, `creative`, `portfolio`]} />
+        <Helmet title={siteTitle} />
+        <section>
+          <h2>Info</h2>
+          <article>
+            <h3>About</h3>
+            <Img
+              alt="Silke Derudder"
+              fluid={about.image.fluid}
+            />
+            <p>I&#39;m <span>Silke</span>, {about.body.body}</p>
+          </article>
+          <article>
+            <h3>Get in touch</h3>
+            <p>Send me an <br/> <a href="mailto:hello@silkederudder.be">email</a></p>
+            <ul>
+              <li><a href="https://github.com/silkederudder">Github</a></li>
+              <li><a href="https://linkedin.com/silkederudder">LinkedIn</a></li>
+              <li><a href="https://www.behance.net/silkederudder">Behance</a></li>
+              <li><a href="https://twitter.com/silkederudder">Twitter</a></li>
+              <li><a href="https://instagram.com/silkederudder">Instagram</a></li>
+              <li><a href="https://dribbble.com/silkederudder">Dribble</a></li>
+            </ul>
+          </article>
+        </section>
+      </Layout>
+    )
+  }
+}
+
+export default AboutPage
+
+export const pageQuery = graphql`
+  query AboutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulPerson {
+      edges {
+        node {
+          body {
+            body
+          }
+          image {
+            file {
+              url
+            }
+            fluid(
+              maxWidth: 1536
+              maxHeight: 1024
+              resizingBehavior: FILL
+              background: "rgb:000000"
+            ) {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
