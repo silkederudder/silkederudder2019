dd5159772a0b97b5d54291adbd2c1f42b774850e
Silke Derudder <silkederudder@hotmail.com>
2019-06-18 16:51:02 +0200

    💄 Use webfontloader

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-web-font-loader`,
+      options: {
+        google: {
+          families: [`Droid Sans`, `Droid Serif`]
+        }
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
