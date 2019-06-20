a6b3875decd56f288d68c47c3cfb354ff0de1c48
Silke Derudder <silkederudder@hotmail.com>
2019-06-20 11:06:44 +0200

    ♻ Use functional components instead of class components, 💄 Add wave hand on hover

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,42 +8,44 @@ import Img from "gatsby-image";
 import base from './base.module.css'
 import styles from './about.module.css'
 
-class AboutPage extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const about = get(this, 'props.data.allContentfulPerson.edges')[0].node
+const handleShowImg = e => {
+  
+}
 
-    return (
-      <Layout>
-        <SEO title="About" keywords={[`info`, `silke derudder`, `design`, `development`, `contact`, `creative`, `portfolio`]} />
-        <Helmet title='Info' />
-        <section className={styles.info}>
-          <h2 className={base.hide}>Info</h2>
-          <article className={styles.info__about}>
-            <h3 className={base.hide}>About</h3>
-            <Img className="info__img" alt="Silke Derudder" fluid={about.image.fluid} />
-            <p className={styles.info__text}>I&#39;m <span className={styles.info__name}>Silke</span>, {about.body.body}</p>
-          </article>
-          <article className={styles.info__contact}>
-            <h3 className={base.hide}>Get in touch</h3>
-            <p className={styles.info__mail}>Send me an <br/> <a href="mailto:hello@silkederudder.be">email</a></p>
-            <ul className={styles.info__socials}>
-              <div>
-                <li className={styles.info__socialItem}><a className={base.link} href="https://github.com/silkederudder">Github</a></li>
-                <li className={styles.info__socialItem}><a className={base.link} href="https://linkedin.com/silkederudder">LinkedIn</a></li>
-                <li className={styles.info__socialItem}><a className={base.link} href="https://www.behance.net/silkederudder">Behance</a></li>
-              </div>
-             <div>
-                <li className={styles.info__socialItem}><a className={base.link} href="https://twitter.com/silkederudder">Twitter</a></li>
-                <li className={styles.info__socialItem}><a className={base.link} href="https://instagram.com/silkederudder">Instagram</a></li>
-                <li className={styles.info__socialItem}><a className={base.link} href="">Resume</a></li>
-             </div>
-            </ul>
-          </article>
-        </section>
-      </Layout>
-    )
-  }
+const AboutPage = (props) => {
+  const siteTitle = get(props, 'data.site.siteMetadata.title')
+  const about = get(props, 'data.allContentfulPerson.edges')[0].node
+
+  return (
+    <Layout>
+      <SEO title="About" keywords={[`info`, `silke derudder`, `design`, `development`, `contact`, `creative`, `portfolio`]} />
+      <Helmet title='Info' />
+      <section className={styles.info}>
+        <h2 className={base.hide}>Info</h2>
+        <article className={styles.info__about}>
+          <h3 className={base.hide}>About</h3>
+          <Img className="info__img" alt="Silke Derudder" fluid={about.image.fluid} />
+          <p className={styles.info__text}>I&#39;m <span className={styles.info__name} onMouseMove={handleShowImg}>Silke</span>, {about.body.body}</p>
+        </article>
+        <article className={styles.info__contact}>
+          <h3 className={base.hide}>Get in touch</h3>
+          <p className={styles.info__mail}>Send me an <br/> <a href="mailto:hello@silkederudder.be">email</a></p>
+          <ul className={styles.info__socials}>
+            <div>
+              <li className={styles.info__socialItem}><a className={base.link} href="https://github.com/silkederudder">Github</a></li>
+              <li className={styles.info__socialItem}><a className={base.link} href="https://linkedin.com/silkederudder">LinkedIn</a></li>
+              <li className={styles.info__socialItem}><a className={base.link} href="https://www.behance.net/silkederudder">Behance</a></li>
+            </div>
+            <div>
+              <li className={styles.info__socialItem}><a className={base.link} href="https://twitter.com/silkederudder">Twitter</a></li>
+              <li className={styles.info__socialItem}><a className={base.link} href="https://instagram.com/silkederudder">Instagram</a></li>
+              <li className={styles.info__socialItem}><a className={base.link} href="">Resume</a></li>
+            </div>
+          </ul>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,52 +8,50 @@ import Img from "gatsby-image";
 import styles from './index.module.css'
 import base from './base.module.css'
 
-class IndexPage extends React.Component {
-  handleTilt(e) {
-    const max = 5;
-    const w = e.currentTarget.clientWidth;
-    const h = e.currentTarget.clientHeight;
+const handleTilt = (e) => {
+  const max = 5;
+  const w = e.currentTarget.clientWidth;
+  const h = e.currentTarget.clientHeight;
 
-    e.currentTarget.style = `will-change: transform; transform: perspective(1000px) rotateY(${(e.pageX / w) * max}deg) rotateX(${(e.pageY / h) * max}deg) scale3d(.9, .9, .9);`;
-  }
+  e.currentTarget.style = `will-change: transform; transform: perspective(1000px) rotateY(${(e.pageX / w) * max}deg) rotateX(${(e.pageY / h) * max}deg) scale3d(.9, .9, .9);`;
+}
 
-  handleReset(e) {
-    e.currentTarget.style = `will-change: transform; transform: perspective(1000px) rotateY(0) rotateX(0) scale3d(1, 1, 1);`;
-  }
+const handleReset = (e) => {
+  e.currentTarget.style = `will-change: transform; transform: perspective(1000px) rotateY(0) rotateX(0) scale3d(1, 1, 1);`;
+}
 
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const projects = get(this, 'props.data.allContentfulProject.edges')
+const IndexPage = (props) => {
+  const siteTitle = get(props, 'data.site.siteMetadata.title')
+  const projects = get(props, 'data.allContentfulProject.edges')
 
-    return (
-      <Layout>
-        <SEO title="Home" keywords={[`projects`, `silke derudder`, `design`, `development`, `contact`, `creative`, `portfolio`]} />
-        <Helmet title={` Projects`} />
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`projects`, `silke derudder`, `design`, `development`, `contact`, `creative`, `portfolio`]} />
+      <Helmet title={` Projects`} />
 
-        <section className={styles.index}>
-          <h2 className={base.hide}>Projects</h2>
-          <ul className={styles.index__projects}>
-            {projects.map(({ node }) => {
-              return (
-                <li key={node.slug} className={styles.index__project}>
-                    <Link to={`/project/${node.slug}`} className={styles.index__projectLink}>
-                      <article className={styles.index__projectItem} onMouseMove={this.handleTilt} onMouseLeave={this.handleReset}>
-                        <div className={styles.index__overlay}></div>
-                        <div className={styles.index__projectInfo}>
-                          <h3 className={styles.index__projectTitle}>{node.title}</h3>
-                          <p className={styles.index__tags}>{node.tags}</p>
-                        </div>
-                        <Img className="index__projectImg" alt={node.title} fluid={node.coverImage.fluid} />
-                      </article>
-                    </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </section>
-      </Layout>
-    )
-  }
+      <section className={styles.index}>
+        <h2 className={base.hide}>Projects</h2>
+        <ul className={styles.index__projects}>
+          {projects.map(({ node }) => {
+            return (
+              <li key={node.slug} className={styles.index__project}>
+                  <Link to={`/project/${node.slug}`} className={styles.index__projectLink}>
+                    <article className={styles.index__projectItem} onMouseMove={handleTilt} onMouseLeave={handleReset}>
+                      <div className={styles.index__overlay}></div>
+                      <div className={styles.index__projectInfo}>
+                        <h3 className={styles.index__projectTitle}>{node.title}</h3>
+                        <p className={styles.index__tags}>{node.tags}</p>
+                      </div>
+                      <Img className="index__projectImg" alt={node.title} fluid={node.coverImage.fluid} />
+                    </article>
+                  </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </Layout>
+  )
 }
 
 export default IndexPage
