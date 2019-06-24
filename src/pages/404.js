52dd270e5d578f49b24b44422f0f88b3ebed1f0c
Silke Derudder <silkederudder@hotmail.com>
2019-06-24 17:20:50 +0200

    💄 Style not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,34 @@
 import React from "react"
-
+import { Link, useStaticQuery, graphql } from "gatsby";
+import Helmet from 'react-helmet'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from './404.module.css'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const url = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "404.gif" }) {
+        publicURL
+      }
+    }
+  `).placeholderImage.publicURL
 
-export default NotFoundPage
+
+  return (
+    <Layout>
+      <SEO title="Not found" keywords={[`not found`, `silke derudder`, `design`, `development`, `contact`, `creative`, `portfolio`]} />
+      <Helmet title='Not found' />
+      <section className={styles.notfound}>
+        <div className={styles.notfound__content}>
+          <h2 className={styles.notfound__title}>404</h2>
+          <h3 className={styles.notfound__text}>Oops, looks like you are lost.</h3>
+          <Link to="/" className={styles.notfound__link}>Go home</Link>
+        </div>
+        <img src={url} alt="Page not found" className={styles.notfound__img}/>
+      </section>
+    </Layout>
+  )
+}
+
+export default NotFoundPage;
