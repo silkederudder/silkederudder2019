7a5dc237f88e01ad0826fe03fcccc4e0f02eea0d
silkederudder <silke.derudder@student.howest.be>
2019-06-28 10:33:15 +0200

    ✨ Add page transitions

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,21 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import styles from './header.module.css'
+import TransitionLink from "gatsby-plugin-transition-link"
 
 const Header = ({ siteTitle }) => (
   <header className={styles.header}>
-    <h1 className={styles.header__title}><Link to='/'>{siteTitle}</Link></h1>
+    <h1 className={styles.header__title}>
+      <TransitionLink exit={{length: 1}} entry={{length: .5}} to='/'>{siteTitle}</TransitionLink>
+    </h1>
     <nav className={styles.header__nav}>
       <ul className={styles.header__list}>
-        <li className={styles.header__listitem}><Link activeClassName="header__nav--active" className={styles.header__link} to="/">Projects</Link></li>
-        <li className={styles.header__listitem}><Link activeClassName="header__nav--active" className={styles.header__link} to="/about">Info</Link></li>
+        <li className={styles.header__listitem}>
+          <TransitionLink exit={{length: 1}} entry={{length: .5}} activeClassName="header__nav--active" className={styles.header__link} to="/">Projects</TransitionLink>
+        </li>
+        <li className={styles.header__listitem}>
+          <TransitionLink exit={{length: 1}} entry={{length: .5}} activeClassName="header__nav--active" className={styles.header__link} to="/about">Info</TransitionLink>
+        </li>
       </ul>
     </nav>
   </header>
