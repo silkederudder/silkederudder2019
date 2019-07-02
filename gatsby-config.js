72fb9c33802876ae092b8bd0b9ae917bdb11cb2e
Silke Derudder <silkederudder@hotmail.com>
2019-07-02 21:08:43 +0200

    :chart_with_upwards_trend: Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,6 +91,12 @@ module.exports = {
       }
     },
     `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.TRACKING_ID,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
